cca41d9ed7d18028a7a1b1ed8d4504e6a8ee6e0c
Arturo Cabrera <arturo.jcr@gmail.com>
2024-07-26 21:18:26 -0400

    agregar sulfuras

---
+++ src/main/java/edu/alenasoft/Sulfuras.java
@@ -0,0 +1,12 @@
+package edu.alenasoft;
+
+public class Sulfuras extends GildedItem {
+  public Sulfuras(Item item) {
+    super(item);
+  }
+
+  @Override
+  public void updateQuality() {
+    // No changes for Sulfuras
+  }
+}
